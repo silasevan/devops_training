5438e7fd40ee069df6c6ec3bce4dc5751f9727c6
silasevan <silasevan@gmail.com>
2025-04-11 22:20:46 +0100

    add readme documentation for advance linux command project

--- shell_scripting/shell-scripting/my_first_shell_script.sh
+++ shell_scripting/shell-scripting/my_first_shell_script.sh
@@ -4,6 +4,8 @@
 mkdir Folder1
 mkdir Folder2
 mkdir Folder3
+# This is a single-line comment in Bash
+echo "Hello, you are learning Bash Scripting on DAREY.IO!" # This is also a comment, following a command
 
 # Create users
 sudo useradd user1
@@ -11,3 +13,7 @@ sudo useradd user2
 sudo useradd user3
 
 
+# This is another way to create
+# a multi-line comment. Each line
+# is prefixed with a # symbol.
+echo "Here is an actual code that gets executed"
